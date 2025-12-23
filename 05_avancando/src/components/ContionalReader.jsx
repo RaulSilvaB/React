9329b54819cc8303184085157ec56ab258f3b7bd
raul <raulsilvab01@gmail.com>
2025-12-23 15:32:16 -0300

    Condiçoes 2

--- 05_avancando/src/components/ContionalReader.jsx
+++ 05_avancando/src/components/ContionalReader.jsx
@@ -2,14 +2,14 @@ import { useState } from "react"
 
 const ConditionalRender = () => {
     const [condicao,setCondicao] = useState(true)
-    const [name] = useState("raul")
+    const [name,setName] = useState("raul")
 
     return(
         <div className="pai">
-            <h1>Renderizador</h1>
+            <h1>Isso sera exibido? "(&&)"</h1>
             <button onClick={() => setCondicao((prevState) => !prevState)}>Exibir / Ocultar</button>
             {condicao && 
-            <h2>Jesus é o caminho e a vida</h2>}
+            <h2>Jesus é o caminho e a vida (if ternário)</h2>}
             {name === "raul" ? (
                 <div>
                     <p>é o raul</p>
@@ -19,6 +19,18 @@ const ConditionalRender = () => {
                     <p>Não é o raul</p>
                 </div>
             )}
+            {name == 'vivian' ? (
+                <div>
+                    <p>Oi é a Vivian?</p>
+                </div>
+                ) : (
+                    <div>
+                        <p>Não é a Vivian</p>
+                    </div>
+                )}
+
+                <button onClick={() => setName('vivian')}>Clica aqui!</button>
+
             </div>
     )
 }
